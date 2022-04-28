c98a1a786aafe58188d0f297232c3ab581e6eb01
HadeelDaragmeh <97829483+HadeelDaragmeh158@users.noreply.github.com>
2022-04-28 12:20:10 +0300

    Update README.m

--- Read26/README.m
+++ Read26/README.m
@@ -10,8 +10,6 @@ You can use c++,java, and kotlin to create Android app.
         - contains the contents of an Android app project including some additional metadata that is not required at runtime.
 
 
-
-
 "The Android system implements the principle of least privilege"
  - It's possible to arrange for two apps to share the same Linux user ID, in which case they are able to access each other's files. To conserve system resources, apps with the same user ID can also arrange to run in the same Linux process and share the same VM. The apps must also be signed with the same certificate.
  - An app can request permission to access device data such as the device's location, camera, and Bluetooth connection. The user has to explicitly grant these permissions. For more information, see Working with System Permissions.
@@ -23,5 +21,3 @@ The rest of this document introduces the following concepts:
  - Resources that are separate from the app code and that allow your app to gracefully optimize its behavior for a variety of device configurations.
 
 # App components
-
-![appComp](https://imgs.search.brave.com/40dZXJ4l7JgyzYKHu0hGhgPMnsgSwdxdv54K6DbD0Wg/rs:fit:844:225:1/g:ce/aHR0cHM6Ly90c2Ux/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5E/S2Jiazd1RE1ndHdm/TUxMaF85bjRBSGFF/SyZwaWQ9QXBp)
